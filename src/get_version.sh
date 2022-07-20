f5bf1fc4a1fb324d099e94adb1abed0e9ad861c9
qiaofeng1227 <76487013@qq.com>
2022-07-20 14:32:43 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "joomla version:" $(docker exec -i $1 cat /data/wwwroot/joomla/language/en-GB/install.xml |grep "<version>" |tr -d "<version>") | sudo tee -a /data/logs/install_version.txt
+sudo echo "joomla version:" $(docker exec -i $1 cat /bitnami/joomla/language/en-GB/install.xml |grep "<version>" |tr -d "<version>") | sudo tee -a /data/logs/install_version.txt
